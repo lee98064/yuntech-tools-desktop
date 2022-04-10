bca6d2c6ace99284e2da5c8e2a425bde02dafd36
Ryan Lee <lee98064@gmail.com>
2022-04-11 01:21:52 +0800

    Fix typescript error

--- src/renderer/nuxt.config.js
+++ src/renderer/nuxt.config.js
@@ -24,16 +24,16 @@ module.exports = {
   modules: [
     '@nuxtjs/vuetify',
   ],
-          vuetify: {
-            theme: {
-              themes: {
-                light: {
-                  primary: '#1867c0',
-                  secondary: '#b0bec5',
-                  accent: '#8c9eff',
-                  error: '#b71c1c',
-                },
-              },
-            }
-          }
+  vuetify: {
+    theme: {
+      themes: {
+        light: {
+          primary: '#1867c0',
+          secondary: '#b0bec5',
+          accent: '#8c9eff',
+          error: '#b71c1c',
+        },
+      },
+    }
+  },
 };
